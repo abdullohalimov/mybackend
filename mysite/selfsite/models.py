2503f10c29150aaba790bef7acac4e41d1a46d01
Abdulloh <abdulloxcobra@gmail.com>
2022-10-09 23:27:33 +0500

    Initial commit

---
+++ mysite/selfsite/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Servicefifteen(models.Model):
+    pay_id = models.IntegerField("Pay ID", max_length=10)
+    date = models.DateTimeField("Date and Time")
+
+    
